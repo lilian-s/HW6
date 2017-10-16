527fcb40f183e122b57db3f057964cf1c6c95dee
Lilian Sheu <lilians@umich.edu>
2017-10-16 16:40:06 -0400

    Finished HW6b

--- solution.py
+++ solution.py
@@ -13,18 +13,22 @@ ctx.verify_mode = ssl.CERT_NONE
 # sample data url: http://py4e-data.dr-chuck.net/known_by_Fikret.html
 # real data url: http://py4e-data.dr-chuck.net/known_by_Aimie.html
 url = input('Enter URL: ')
-html = urllib.request.urlopen(url, context=ctx).read()
 
-count = input('Enter count: ')
-pos = input('Enter position: ')
+count = int(input('Enter count: '))
+pos = int(input('Enter position: '))
 
 # html.parser is the HTML parser included in the standard Python 3 library.
 # information on other HTML parsers is here:
 # http://www.crummy.com/software/BeautifulSoup/bs4/doc/#installing-a-parser
-soup = BeautifulSoup(html, "html.parser")
+
 
 # Retrieve all of the anchor tags
-tags = soup('a')
-for tag in tags:
-	print(tag.contents[0])
-    #print(tag.get('href', None))
+last_output = ''
+for i in range(count):
+	html = urllib.request.urlopen(url, context=ctx).read()
+	soup = BeautifulSoup(html, "html.parser")
+	tags = soup('a')
+	last_output = tags[pos-1].contents[0]
+	url = tags[pos-1].get('href', None)
+    #print(tags[i].attrs)
+print(last_output)
